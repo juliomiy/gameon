d1f19b7dc3d80cfc71d553079955e8b669714de5
julio hernandez-miyares <julio@jttr.com>
2010-07-12 12:15:39 -0400

    Sync with macbook pro July 12 2010

--- src/com/jittr/android/foursquare/GOFoursquareWrapper.java
+++ src/com/jittr/android/foursquare/GOFoursquareWrapper.java
@@ -1,5 +1,18 @@
 package com.jittr.android.foursquare;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHttpResponse;
+
+//import org.apache.commons.httpclient.util.HttpURLConnection;
+
 import oauth.signpost.basic.DefaultOAuthConsumer;
 import oauth.signpost.basic.DefaultOAuthProvider;
 import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
@@ -8,6 +21,7 @@ import oauth.signpost.exception.OAuthCommunicationException;
 import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
 import oauth.signpost.exception.OAuthNotAuthorizedException;
+import oauth.signpost.http.HttpResponse;
 import android.content.Context;
 
 import com.jittr.android.gamemanager.GameOnProperties;
@@ -31,6 +45,9 @@ public class GOFoursquareWrapper {
 	private String accessToken;
 	private String accessTokenSecret;
 	
+	private int lastStatusCode = -1;
+	private String lastStatus;
+	
 	/* prepare the object - currently the Oauth tokens are hardcoded via the defaultValue in the retrieveSharedPreference call
 	 * TODO - replace the defaultValue with Null.
 	 */
@@ -42,6 +59,8 @@ public class GOFoursquareWrapper {
    	     consumer = new CommonsHttpOAuthConsumer(consumerKey,consumerSecretKey);
          accessToken = gameOnProperties.retrieveSharedPreference("GAMEON_FOURSQUARE_OAUTH_TOKEN","AQM0W1I4W5RLVPLKD2JCM0CFOAPGJFNKLLQFDFAS4X4U1EK4");
          accessTokenSecret = gameOnProperties.retrieveSharedPreference("GAMEON_FOURSQUARE_OAUTH_TOKEN_SECRET","Z124AVQQHMPEMDHBWWDIUPFQHTYTGE3UZ2GY1HUAOUXEJGJK");
+         if (isAuthorized()==true) consumer.setTokenWithSecret(accessToken, accessTokenSecret);
+
 	} //constructor
 
 	/* return the url to take the user to grant rights foursquare page. If user selected  "Approve" , Gameon
@@ -79,6 +98,97 @@ public class GOFoursquareWrapper {
 	    	return true;
 	}  //is Authorized
 	
+	public boolean venueDetailAPI(String vid) {
+		String api = "http://api.foursquare.com/v1/venue?vid=" + vid;
+		BasicHttpResponse response = null;
+        
+		HttpGet request  = signRequest(api);
+        if (null == request) return false;
+        
+ 		HttpClient httpClient = new DefaultHttpClient();
+        try {
+        	//httpClient.
+			response =  (BasicHttpResponse) httpClient.execute(request);
+			lastStatusCode = response.getStatusLine().getStatusCode();
+			lastStatus = response.getStatusLine().getReasonPhrase();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return (null == response ? false: true);
+	}
+	
+	public boolean userDetailAPI() {
+    	// send the request
+		String api = "http://api.foursquare.com/v1/user";
+		BasicHttpResponse response = null;
+        
+		HttpGet request  = signRequest(api);
+        if (null == request) return false;
+        
+ 		HttpClient httpClient = new DefaultHttpClient();
+        try {
+        	//httpClient.
+			response =  (BasicHttpResponse) httpClient.execute(request);
+			lastStatusCode = response.getStatusLine().getStatusCode();
+			lastStatus = response.getStatusLine().getReasonPhrase();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return (null == response ? false: true);
+	}
+ /* Checkin API */
+	public boolean checkIns() {
+    	// send the request
+		String api = "http://api.foursquare.com/v1/checkins";
+		BasicHttpResponse response = null;
+        
+		HttpGet request  = signRequest(api);
+        if (null == request) return false;
+        
+ 		HttpClient httpClient = new DefaultHttpClient();
+        try {
+        	//httpClient.
+			response =  (BasicHttpResponse) httpClient.execute(request);
+			lastStatusCode = response.getStatusLine().getStatusCode();
+			lastStatus = response.getStatusLine().getReasonPhrase();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return (null == response ? false: true);
+    }  //checkins
+    
+	/* sign an http request with accessToken,AccessTokenSecret */
+	public HttpGet signRequest(String strUrl) {
+		if (isAuthorized() != true) return null;
+		HttpGet request = null;
+		try {
+			request = new HttpGet(strUrl);
+	        // sign the request
+	        consumer.sign(request);
+		} catch (OAuthMessageSignerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OAuthExpectationFailedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OAuthCommunicationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return request;
+	}
 	public String getConsumerKey() {
 		return consumerKey;
 	}
@@ -102,4 +212,27 @@ public class GOFoursquareWrapper {
             this.accessToken= accessToken;
             this.accessTokenSecret=accessTokenSecret;
 	 } //storeAccessToken
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+		
+	}
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public String getAccessTokenSecret() {
+		return accessTokenSecret;
+	}
+
+	public void setAccessTokenSecret(String accessTokenSecret) {
+		this.accessTokenSecret=accessTokenSecret;
+	}
+	
+	public int getLastStatusCode() {
+        return lastStatusCode;
+	}
+	public String getLastStatus() {
+		return lastStatus;
+	}
 }  //class
